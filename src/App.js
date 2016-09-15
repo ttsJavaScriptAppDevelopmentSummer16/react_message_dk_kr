667b1335902ecff396335ba883f552dc959f295d
Kevin Rocker <kevin.j.rocker@gmail.com>
2016-09-15 19:17:35 -0400

    Lots more changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import MessageList from 'messages.js'
+import MessageList from './messages.js'
 import logo from './logo.svg';
 import './App.css';
 
@@ -12,7 +12,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <MessageList />
         </p>
       </div>
     );
--- src/messages.js
+++ src/messages.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
 
 class Message extends Component {
-  constructor(props){
-    super(props)
-  }
-
-  belongsToCurrentUser = () => {
-    return this.props.currentUser === this.props.message.user
-  }
-
   render() {
-    content = `Here is the message: ${this.props.message.text}, ` +
+    let content = `Here is the message: ${this.props.message.text}, ` +
               `at time: ${this.props.message.time}, from user: ${this.props.message.user}`
     return (
       <li key={this.props.key}>
-        <h4 style={belongsToCurrentUser ? style.red : style.norm}>{content}</h4>
+        <h4 style={this.props.style}>{content}</h4>
       </li>
+    )
+  }
+}
+
+class LoadButton extends Component {
+  render() {
+    let text = this.props.val ? "Load Less" : "Load More"
+    return (
+      <button style={{marginLeft: '70%'}}
+        type="button" onClick={this.props.func}>{text}</button>
     )
   }
 }
@@ -29,55 +31,63 @@ class MessageList extends Component {
                   {text:"This is message 2",time:"2:00AM",user:"William"},
                   {text:"This is message 3",time:"3:00AM",user:"Nathan"} ],
       currentUser: "Toki",
-      loadMore: false }
+      loadMore: false,
+      newMessage: null
+    }
   }
 
-  addMsg = (text, event) => {
-    let currentMessages = this.state.messages
-    let time = new Date().toTimeString()
-    let user = this.state.currentUser
-    let newMessage = {text, time, user}
-    currentMessages.push(newMessage)
-    this.setState({messages: currentMessages})
+  updateMessage = (e) => {
+    this.setState({newMessage: e.target.value})
   }
 
-  loadAll = (event) => {
-    this.setState({loadMore: true})
+  // editMessage = () => {
+  //   this.setState({editMessage: true})
+  // }
+
+  addMessage = (e) => {
+    e.preventDefault();
+    if (!!this.state.newMessage) {
+      let messages = this.state.messages.slice()
+      let content = this.state.newMessage
+      let timestamp = new Date().toTimeString()
+      messages.push({text: content, time: timestamp, user: this.state.currentUser})
+      this.setState({messages, newMessage: null})
+    }
+    else {
+      alert('Please Enter A Message')
+    }
+  }
+
+  loadSwitch = (event) => {
+    let currentLoad = this.state.loadMore
+    this.setState({loadMore: !currentLoad})
+  }
+
+  belongsToCurrentUser = (message) => {
+    return this.state.currentUser === message.user
   }
 
   render() {
 
-    var messageMap = this.state.messages.map(function(message, i){
-        return (<Message key={i} message={message} />)
+    let messageMap = this.state.messages.map(function(message, i){
+      let message_style = this.belongsToCurrentUser(message) ? style.red : style.norm
+      return (<Message key={i} message={message} style={message_style}/>)
     })
+
+    messageMap = this.state.loadMore ? messageMap : messageMap.slice(0,3)
 
     return (
       <div className="messages">
         <ul> {messageMap} </ul>
-        <button style={{marginLeft: '70%', hidden: this.state.messages.length > 3}}
-          type="button" onClick={this.loadAll}>Load More</button>
-        <label htmlfor='messageInput'>Insert Message:
-          <input type = 'text' id= 'messageInput' ref='messageInput' placeholder='Your message here'/>
-        </label>
-        <input type = 'submit' value = 'Submit'/>
-
+        <LoadButton func={this.loadSwitch} val={this.state.loadMore} />
+        <form onSubmit={this.addMessage}>
+          <label htmlFor='messageInput'>Message Input</label>
+          <input type='text' name='messageInput' value={this.state.newMessage} onChange={this.updateMessage} id='messageInput'/><br />
+          <button type="submit" className='primary'>Add Message</button>
+        </form>
       </div>
     );
   }
 }
 
-class App extends Component {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        <MessageList />
-      </p>
-    </div>
-  );
-}
-
-export default App;
+export default MessageList;
