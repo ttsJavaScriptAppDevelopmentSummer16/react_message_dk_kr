9ed96bdbfa01a2deef9004d4a8b34e80eacd54ec
Kevin Rocker <kevin.j.rocker@gmail.com>
2016-09-15 20:37:51 -0400

    Finally working

--- src/messages.js
+++ src/messages.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 
 class Message extends Component {
   render() {
-    let content = `Here is the message: ${this.props.message.text}, ` +
-              `at time: ${this.props.message.time}, from user: ${this.props.message.user}`
+    let content = `${this.props.message.text}, ` +
+              `from user: ${this.props.message.user}`
     return (
-      <li key={this.props.key}>
-        <h4 style={this.props.style}>{content}</h4>
+      <li>
+        <h4 style={{color: this.props.style}}>{content}</h4>
       </li>
     )
   }
@@ -27,12 +27,22 @@ class MessageList extends Component {
   constructor() {
     super()
     this.state = {
-      messages: [ {text:"This is message 1",time:"1:00AM",user:"Toki"},
-                  {text:"This is message 2",time:"2:00AM",user:"William"},
-                  {text:"This is message 3",time:"3:00AM",user:"Nathan"} ],
-      currentUser: "Toki",
+      messages: [ {text:"This is message 1",time:"1:00AM",user:"Skwisgelf"},
+                  {text:"This is message 2",time:"1:00AM",user:"Taller"},
+                  {text:"This is message 3",time:"1:00AM",user:"Than A"},
+                  {text:"This is message 4",time:"2:00AM",user:"Tree"},
+                  {text:"This is message 5",time:"1:00AM",user:"Toki"},
+                  {text:"This is message 6",time:"1:00AM",user:"Wartooth"},
+                  {text:"This is message 7",time:"1:00AM",user:"Bumblebee"},
+                  {text:"This is message 8",time:"2:00AM",user:"William"},
+                  {text:"This is message 9",time:"2:00AM",user:"Murderface"},
+                  {text:"This is message 10",time:"2:00AM",user:"Murderface"},
+                  {text:"This is message 11",time:"2:00AM",user:"Murderface"},
+                  {text:"This is message 12",time:"3:00AM",user:"Nathan"},
+                  {text:"This is message 13",time:"3:00AM",user:"Explosion"} ],
+      currentUser: "Murderface",
       loadMore: false,
-      newMessage: null
+      newMessage: ''
     }
   }
 
@@ -51,7 +61,7 @@ class MessageList extends Component {
       let content = this.state.newMessage
       let timestamp = new Date().toTimeString()
       messages.push({text: content, time: timestamp, user: this.state.currentUser})
-      this.setState({messages, newMessage: null})
+      this.setState({messages, newMessage: ''})
     }
     else {
       alert('Please Enter A Message')
@@ -63,14 +73,12 @@ class MessageList extends Component {
     this.setState({loadMore: !currentLoad})
   }
 
-  belongsToCurrentUser = (message) => {
-    return (this.state.currentUser === message.user)
-  }
-
   render() {
 
+    const currentUser = this.state.currentUser
+
     let messageMap = this.state.messages.map(function(message, i){
-      let message_style = belongsToCurrentUser(message) ? style.red : style.norm
+      let message_style = (currentUser === message.user) ? 'red' : 'black'
       return (<Message key={i} message={message} style={message_style}/>)
     })
 
@@ -78,10 +86,10 @@ class MessageList extends Component {
 
     return (
       <div className="messages">
-        <ul> {messageMap} </ul>
         <LoadButton func={this.loadSwitch} val={this.state.loadMore} />
+        <ul> {messageMap} </ul>
         <form onSubmit={this.addMessage}>
-          <label htmlFor='messageInput'>Message Input</label>
+          <label htmlFor='messageInput'>Message Input</label><br/>
           <input type='text' name='messageInput' value={this.state.newMessage} onChange={this.updateMessage} id='messageInput'/><br />
           <button type="submit" className='primary'>Add Message</button>
         </form>
